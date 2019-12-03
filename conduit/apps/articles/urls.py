5ece725527b0f8551ca607d4f5643ee9f0791aef
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-04 02:38:41 +0800

    Django update version

--- conduit/apps/articles/urls.py
+++ conduit/apps/articles/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
@@ -9,18 +9,13 @@ from .views import (
 )
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'articles', ArticleViewSet)
+router.register('^', ArticleViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-
-    url(r'^articles/feed/?$', ArticlesFeedAPIView.as_view()),
-
-    url(r'^articles/(?P<article_slug>[-\w]+)/favorite/?$',  ArticlesFavoriteAPIView.as_view()),
-
-    url(r'^articles/(?P<article_slug>[-\w]+)/comments/?$',  CommentsListCreateAPIView.as_view()),
-
-    url(r'^articles/(?P<article_slug>[-\w]+)/comments/(?P<comment_pk>[\d]+)/?$',  CommentsDestroyAPIView.as_view()),
-
-    url(r'^tags/?$', TagListAPIView.as_view()),
+    path('articles/', include(router.urls)),
+    path('articles/feed/', ArticlesFeedAPIView.as_view()),
+    path('articles/:article_slug/favorite/',  ArticlesFavoriteAPIView.as_view()),
+    path('articles/:article_slug/comments/',  CommentsListCreateAPIView.as_view()),
+    path('articles/:article_slug/comments/:comment_pk/',  CommentsDestroyAPIView.as_view()),
+    path('tags/', TagListAPIView.as_view()),
 ]
--- conduit/apps/profiles/urls.py
+++ conduit/apps/profiles/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import ProfileRetrieveAPIView, ProfileFollowAPIView
 
 urlpatterns = [
-    url(r'^profiles/(?P<username>\w+)/?$', ProfileRetrieveAPIView.as_view()),
-    url(r'^profiles/(?P<username>\w+)/follow/?$', ProfileFollowAPIView.as_view()),
+    path('profiles/:username/', ProfileRetrieveAPIView.as_view()),
+    path('profiles/:username/follow/', ProfileFollowAPIView.as_view()),
 ]
